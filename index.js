5a8964267faa21ee0c0ff49c9afd2381dc33f89b
charles yarney <charlesyarney@gmail.com>
2021-11-15 15:23:39 +0000

    completed challenge and algorithm

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+//declare function called findLongestWord
+//function should take in an array of strings
+// initialize parameter(words)
+//declare a variable called longestWord should compare the length all strings in array
+//return the first longest string in array
 //write your code here
+const findLongestWord = (words) =>{
+  let longestWord = "";
+  //for (let i = 0; i > words.length; i++) {
+  //let word = words[i]
+  //if (word.length > longestWord.length) {
+    //longestWord = word;
+    //}
+  for (let word of words){  
+    if (word.length > longestWord.length) {
+    longestWord = word;
+    }
+  }
+  return longestWord;
+};
+console.log(findLongestWord(['jazzy', 'jumpy', 'quaky']));
